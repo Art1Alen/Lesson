a4065be060cfc7932f0852f993ca23e3e3ddb709
artem <cookgamer@inbox.ru>
2020-11-13 23:04:55 +0900

    Test Lesson_1
    Lesson_1

---
+++ Lesson_1/ИМТ/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ИМТ
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Это калькулятор массы тебя ИМТ");
+            Console.WriteLine("Ваш рост");
+            int h = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваш вес");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int v = m / (h * h);
+            int i = v;
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
