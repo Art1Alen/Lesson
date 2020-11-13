7e05c704a9e98788d5d0ed64374fb19293525c85
artem <cookgamer@inbox.ru>
2020-11-13 23:10:06 +0900

    Test Lesson_2
    Lesson_2

---
+++ Lesson_2/Class2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Мое_Задание_2
+{
+    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+    class Class2
+    {
+        static void Main()
+        {
+         
+
+            double score = 0;
+            double input = 0;
+            bool flag = false;
+
+
+            while (true) 
+                while (!flag)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Сумма всех числе {score}");
+                    Console.Write("Повторите ввод числа: "); 
+
+                    flag = double.TryParse(Console.ReadLine(), out input); 
+                }
+            }
+
+
+                    Console.WriteLine("Итоговая сумма всех чисел: {score}");// я чето тут нечего не понял :(( но походу я думаю так
+          
+private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
+    }
+
+
+    }
---
+++ Lesson_2/Class4.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Мое_Задание_2
+{
+    class Class4
+    {
+
+    }
+}
