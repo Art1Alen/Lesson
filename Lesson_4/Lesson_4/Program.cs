b3a43b1c66f29c83014d729ecf35b2a0a6e183a4
artem <cookgamer@inbox.ru>
2020-11-17 18:59:00 +0900

    Create Program.cs

---
+++ Lesson_4/Lesson_4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+// 3 Задача
+
+namespace Lesson_4 
+{
+    class Program
+    {
+        static void Main()
+        {
+            string login = "GeekBrains";
+            string password = "123456789";
+
+            int count = 0;
+            do
+            {
+                Console.WriteLine("\nВведите логин: ");
+                string checkLogin = Console.ReadLine();
+
+                Console.WriteLine("Введите пароль: ");
+                string checkPassword = Console.ReadLine();
+
+
+                if (login == checkLogin && password == checkPassword)
+                {
+
+                    Console.WriteLine("Добро пожаловать");
+                    Console.ReadLine();
+                    break;
+                }
+                Console.WriteLine("Неверно введен логин или пароль");
+                Console.ReadLine();
+                ++count;
+            } while (count < 3);
+
+
+
+        }
+    }
+}
+
+
